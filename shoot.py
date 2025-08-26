375b91808043e63eeaedf690819b3f83d8f0ae84
Jess <olson.jess.m@gmail.com>
2025-08-25 23:40:29 -0600

    Initial commit

---
+++ shoot.py
@@ -0,0 +1,16 @@
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = 300
+    apple.y = 200
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+
+place_apple()
